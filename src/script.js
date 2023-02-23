581fc7162186bb8aaf36dadc9fa7300d902d2b33
anika-jane <112495457+anika-jane@users.noreply.github.com>
2023-02-23 14:21:33 -0500

    Added files to repository

---
+++ src/script.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+function displayTemperature(response) {
+    let temperatureElement = document.querySelector("#now-temp");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#current-date-time");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = "Friday, 5:00";
+}
+
+const apiKey = "4bd54384cf14cca162b7f2304d56c5ce";
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
+
+
+
