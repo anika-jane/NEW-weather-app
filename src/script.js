902f28ca65bbdac35d6813ea78d47e303bb193db
anika-jane <112495457+anika-jane@users.noreply.github.com>
2023-02-27 09:48:58 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -27,7 +27,7 @@ function displayTemperature(response) {
 
     celsiusTemperature = response.data.main.temp;
 
-    temperatureElement.innerHTML = Math.round(celciusTemperature);
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
